fd7d7df799059fecda96bc783b8cacfba29887e4
Robert Strandh <robert.strandh@gmail.com>
2017-02-28 11:44:43 +0100

    Improve code layout by cutting long lines.

--- io.lisp
+++ io.lisp
@@ -27,26 +27,31 @@
   (:report (lambda (condition stream)
              (format stream "Buffer ~A contains non-character object"
                      (name (buffer condition)))))
-  (:documentation "This error is signalled whenever an attempt is
-made to save a buffer that contains a non-character object."))
+  (:documentation
+   #.(format nil "This error is signalled whenever an attempt is made~@
+                  to save a buffer that contains a non-character object.")))
 
 (defun buffer-contains-noncharacter (buffer filepath)
-  "Signal an error of type `buffer-contains-noncharacter' with
-the buffer `buffer' and the filepath `filepath'."
+  #.(format nil "Signal an error of type BUFFER-CONTAINS-NONCHARACTER~@
+                 with the buffer BUFFER and the filepath FILEPATH.")
   (error 'buffer-contains-noncharacter :buffer buffer :filepath filepath))
 
-(defmethod check-buffer-writability ((application-frame climacs) (filepath pathname)
+(defmethod check-buffer-writability ((application-frame climacs)
+                                     (filepath pathname)
                                      (buffer drei-buffer))
   (do-buffer-region (object offset buffer 0 (size buffer))
     (unless (characterp object)
       (buffer-contains-noncharacter buffer filepath)))
   (call-next-method))
 
-(defmethod frame-save-buffer-to-stream ((application-frame climacs) (buffer climacs-buffer) stream)
+(defmethod frame-save-buffer-to-stream ((application-frame climacs)
+                                        (buffer climacs-buffer)
+                                        stream)
   (let ((seq (buffer-sequence buffer 0 (size buffer))))
     (if (every #'characterp seq)
         (write-sequence seq stream)
-        (display-message "Cannot save to file, buffer contains non-character object"))))
+        (display-message
+         "Cannot save to file, buffer contains non-character object"))))
 
 (defun input-from-stream (stream buffer offset)
   (let* ((seq (make-string (file-length stream)))
