1fdd9e6f319f5623e135e7d22ea7336a3a2fea4b
Robert Strandh <robert.strandh@gmail.com>
2017-03-01 11:47:52 +0100

    Remove package definition for Prolog syntax from general package definitions.

--- packages.lisp
+++ packages.lisp
@@ -173,17 +173,6 @@
 	:drei-syntax :flexichain :drei :drei-fundamental-syntax
         :esa-utils))
 
-(defpackage #:climacs-prolog-syntax
-  (:use :clim-lisp :clim :drei-buffer :drei-base
-	:drei-syntax :flexichain :drei :climacs-core :drei-fundamental-syntax
-        :drei :esa-utils)
-  (:shadow #:atom
-           #:close
-           #:exp
-           #:integer
-           #:open
-           #:variable))
-
 (defpackage #:climacs-cl-syntax
   (:use :clim-lisp :clim :drei-buffer :drei-base
 	:drei-syntax :flexichain :drei :drei-fundamental-syntax)
