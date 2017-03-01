2a4d574dda0e58e536464a59a9aa4e478b6cda79
Robert Strandh <robert.strandh@gmail.com>
2017-03-01 11:08:55 +0100

    Remove package definition for C syntax from general package definitions.

--- packages.lisp
+++ packages.lisp
@@ -195,16 +195,6 @@
   (:export #:structedit-mode
            #:structedit-table))
 
-(defpackage #:climacs-c-syntax
-  (:use :clim-lisp :clim :clim-extensions :drei-buffer :drei-base
-	:drei-syntax :drei-fundamental-syntax :flexichain :drei
-	:drei-motion :drei-editing :esa-utils :esa :drei-core :esa-io
-	:drei-lr-syntax)
-  (:shadow clim:form)
-  (:export #:c-syntax)
-  (:documentation "Implementation of the syntax module used for
-editing C code."))
-
 (defpackage #:climacs-java-syntax
   (:use :clim-lisp :clim :clim-extensions :drei-buffer :drei-base
 	:drei-syntax :drei-fundamental-syntax :flexichain :drei
