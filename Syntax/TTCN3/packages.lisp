dfeb658cb4c36946685a44bfba51159f553120be
Robert Strandh <robert.strandh@gmail.com>
2017-03-01 11:22:49 +0100

    New component containing package definition for TTCN3 syntax module.

---
+++ Syntax/TTCN3/packages.lisp
@@ -0,0 +1,6 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:climacs-ttcn3-syntax
+  (:use :clim-lisp :clim :clim-extensions :drei-buffer :drei-base
+	:drei-syntax :flexichain :drei :drei-fundamental-syntax)
+  (:export))
