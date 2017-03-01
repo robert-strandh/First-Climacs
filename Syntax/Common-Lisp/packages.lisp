0d995874cacf2c0563e6c881007463e80053c605
Robert Strandh <robert.strandh@gmail.com>
2017-03-01 12:03:48 +0100

    New component containing package definition for CL syntax module.

---
+++ Syntax/Common-Lisp/packages.lisp
@@ -0,0 +1,6 @@
+(cl:in-package #:common-lisp-user)
+
+(defpackage #:climacs-cl-syntax
+  (:use :clim-lisp :clim :drei-buffer :drei-base
+	:drei-syntax :flexichain :drei :drei-fundamental-syntax)
+  (:export))
